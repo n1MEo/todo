551d542a3c80fd9b01347fdf0d4b6c0602c49911
n1MEo <emo4ka1337@gmail.com>
2025-11-02 17:28:15 +0500

    todo_version1.2

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-document.getElementById('themeToggle').addEventListener('click', function(){
+document.getElementById('themeToggle').addEventListener('click', function () {
     const currentTheme = document.body.className;
     const ThemeIcon = document.getElementById('ThemeIcon');
-    if(currentTheme === 'light-theme'){
+    if (currentTheme === 'light-theme') {
         document.body.className = 'dark-theme';
-        ThemeIcon.src = "sun.svg";
+        ThemeIcon.src = "photos/sun.svg";
     } else {
         document.body.className = 'light-theme';
-        ThemeIcon.src = "moon.svg";
+        ThemeIcon.src = "photos/moon.svg";
     }
 });
+
+const checkboxes = document.querySelectorAll('.task');
+
+checkboxes.forEach(checkbox => {
+    const checkboxText = checkbox.querySelector('.task_text') ||
+        checkbox.nextElementSibling;
+
+    checkbox.addEventListener("click", function () {
+        if (this.checked) {
+            checkboxText.style.textDecoration = 'line-through';
+        } else {
+            checkboxText.style.textDecoration = 'none';
+            }
+    });
+});
+
+
